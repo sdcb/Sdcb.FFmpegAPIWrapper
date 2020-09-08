d459b8385e17495e49cb0555041d17f7e0861b9c
sdcb <sdflysha@qq.com>
2020-09-08 14:35:04 +0800

    Add MediaDictionary/AVDictionary

---
+++ Sdcb.FFmpegAPIWrapper/Common/MediaDictionary/MediaDictionary.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using FFmpeg.AutoGen;
+using static FFmpeg.AutoGen.ffmpeg;
+
+namespace Sdcb.FFmpegAPIWrapper.Common
+{
+    /// <summary>
+    /// <see cref="AVDictionary"/>
+    /// </summary>
+    public unsafe class MediaDictionary : FFmpegHandle, IDictionary<string, string>
+    {
+        private MediaDictionary(AVDictionary* dict) : base((IntPtr)dict) { }
+
+        public static MediaDictionary CreateEmpty() => new MediaDictionary(null);
+
+        public static MediaDictionary Copy(MediaDictionary source)
+        {
+            AVDictionary* destination = null;
+            av_dict_copy(&destination, source, 0).ThrowIfError();
+            return new MediaDictionary(destination);
+        }
+
+        public static MediaDictionary FromDictionary(Dictionary<string, string> dict)
+        {
+            MediaDictionary md = CreateEmpty();
+            foreach (var entry in dict)
+            {
+                md[entry.Key] = entry.Value;
+            }
+            return md;
+        }
+
+        public static implicit operator AVDictionary*(MediaDictionary dict) => (AVDictionary*)dict._handle;
+
+        #region IDictionary<string, string> entries
+        public ICollection<string> Keys => this.Select(x => x.Key).ToArray();
+
+        public ICollection<string> Values => this.Select(x => x.Key).ToArray();
+
+        public int Count => av_dict_count(this);
+
+        public bool IsReadOnly => false;
+
+        public string this[string key]
+        {
+            get
+            {
+                AVDictionaryEntry* entry = av_dict_get(this, key, null, (int)MediaDictionaryFlags.CaseSensitive);
+                if (entry == null)
+                {
+                    throw new KeyNotFoundException(key);
+                }
+
+                return Marshal.PtrToStringUTF8((IntPtr)entry->value);
+            }
+            set
+            {
+                AVDictionary* ptr = this;
+                av_dict_set(&ptr, key, value, 0).ThrowIfError();
+                _handle = (IntPtr)ptr;
+            }
+        }
+
+        public void Add(string key, string value)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+            if (value == null) throw new ArgumentNullException(nameof(value)); // in AVDictionary, value is also not-null.
+
+            AVDictionary* ptr = this;
+            av_dict_set(&ptr, key, value, (int)MediaDictionaryFlags.NoOverwrite).ThrowIfError();
+            _handle = (IntPtr)ptr;
+        }
+
+        public bool ContainsKey(string key)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            AVDictionaryEntry* entry = av_dict_get(this, key, null, (int)MediaDictionaryFlags.CaseSensitive);
+            return entry != null;
+        }
+
+        public bool Remove(string key)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            AVDictionary* ptr = this;
+            bool containsKey = ContainsKey(key);
+
+            av_dict_set(&ptr, key, null, (int)MediaDictionaryFlags.NoOverwrite).ThrowIfError();
+            _handle = (IntPtr)ptr;
+            return containsKey;
+        }
+
+        public bool TryGetValue(string key, out string value)
+        {
+            AVDictionaryEntry* entry = av_dict_get(this, key, null, (int)MediaDictionaryFlags.CaseSensitive);
+            if (entry == null)
+            {
+                value = null;
+                return false;
+            }
+            else
+            {
+                value = Marshal.PtrToStringUTF8((IntPtr)entry->value);
+                return true;
+            }
+        }
+
+        void ICollection<KeyValuePair<string, string>>.Add(KeyValuePair<string, string> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        bool ICollection<KeyValuePair<string, string>>.Contains(KeyValuePair<string, string> item)
+        {
+            return TryGetValue(item.Key, out string value) && value == item.Value;
+        }
+
+        bool ICollection<KeyValuePair<string, string>>.Remove(KeyValuePair<string, string> item)
+        {
+            if (TryGetValue(item.Key, out string value) && value == item.Value)
+            {
+                Remove(item.Key);
+                return true;
+            }
+            return false;
+        }
+
+        public void Clear() => Close();
+
+        void ICollection<KeyValuePair<string, string>>.CopyTo(KeyValuePair<string, string>[] array, int arrayIndex)
+        {
+            if (array == null) throw new ArgumentNullException(nameof(array));
+            if (arrayIndex > array.Length) throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+            if (array.Length - arrayIndex < Count) throw new ArgumentOutOfRangeException(nameof(arrayIndex));
+
+            foreach (KeyValuePair<string, string> entry in this)
+            {
+                array[arrayIndex++] = entry;
+            }
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            IntPtr opaque = AllocOpaque();
+            try
+            {
+                while ((opaque = av_dict_get_safe(this, opaque)) != IntPtr.Zero)
+                {
+                    yield return GenerateResult(opaque);
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(opaque);
+            }
+
+            static KeyValuePair<string, string> GenerateResult(IntPtr ptr)
+            {
+                var entry = (AVDictionaryEntry*)ptr;
+                return KeyValuePair.Create(
+                    Marshal.PtrToStringUTF8((IntPtr)entry->key),
+                    Marshal.PtrToStringUTF8((IntPtr)entry->value));
+            }
+
+            static IntPtr av_dict_get_safe(MediaDictionary dict, IntPtr prev)
+            {
+                return (IntPtr)av_dict_get(dict, "", (AVDictionaryEntry*)prev, (int)MediaDictionaryFlags.IgnoreSuffix);
+            }
+
+            static IntPtr AllocOpaque()
+            {
+                IntPtr address = Marshal.AllocHGlobal(IntPtr.Size);
+                (*(IntPtr*)(address)) = new IntPtr(0);
+                return address;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        #endregion
+
+        protected override void Close()
+        {
+            AVDictionary* p = this;
+            av_dict_free(&p);
+            _handle = IntPtr.Zero;
+        }
+    }
+}
---
+++ Sdcb.FFmpegAPIWrapper/Common/MediaDictionary/MediaDictionaryFlags.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static FFmpeg.AutoGen.ffmpeg;
+
+namespace Sdcb.FFmpegAPIWrapper.Common
+{
+    [Flags]
+    public enum MediaDictionaryFlags : int
+    {
+        /// <summary>
+        /// <see cref="AV_DICT_MATCH_CASE"/>
+        /// </summary>
+        CaseSensitive = AV_DICT_MATCH_CASE,
+
+        /// <summary>
+        /// <see cref="AV_DICT_IGNORE_SUFFIX"/>
+        /// </summary>
+        IgnoreSuffix = 2,
+
+        //AV_DICT_DONT_STRDUP_KEY = 4, // Not suppord in managed code
+        //AV_DICT_DONT_STRDUP_VAL = 8, // Not suppord in managed code
+
+        /// <summary>
+        /// <see cref="AV_DICT_DONT_OVERWRITE"/>
+        /// </summary>
+        NoOverwrite = 16,
+
+        /// <summary>
+        /// <see cref="AV_DICT_APPEND"/>
+        /// </summary>
+        AppendValue = AV_DICT_APPEND,
+
+        /// <summary>
+        /// <see cref="AV_DICT_MULTIKEY"/>
+        /// </summary>
+        AllowSameKey = AV_DICT_MULTIKEY,
+    }
+}
