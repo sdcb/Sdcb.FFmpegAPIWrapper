81ef3bd2f670052a1ce59458de09f699fcc1bc20
sdcb <sdflysha@qq.com>
2020-09-16 15:32:34 +0800

    complete AVCodecParameter.

--- Sdcb.FFmpegAPIWrapper/MediaCodecs/CodecParameters.cs
+++ Sdcb.FFmpegAPIWrapper/MediaCodecs/CodecParameters.cs
@@ -8,6 +8,19 @@ namespace Sdcb.FFmpegAPIWrapper.MediaCodecs
     public unsafe partial class CodecParameters : FFmpegHandle
     {
         /// <summary>
+        /// <see cref="avcodec_parameters_alloc"/>
+        /// </summary>
+        public static CodecParameters Create() => new CodecParameters(avcodec_parameters_alloc(), isOwner: true);
+
+        /// <summary>
+        /// <see cref="avcodec_parameters_copy(AVCodecParameters*, AVCodecParameters*)"/>
+        /// </summary>
+        public void Copy(CodecParameters source)
+        {
+            avcodec_parameters_copy(this, source).ThrowIfError();
+        }
+
+        /// <summary>
         /// <see cref="avcodec_parameters_free(AVCodecParameters**)"/>
         /// </summary>
         public override void Close()
