1b09b0888df0634379ddab5cdae64327333299b6
sdcb <sdflysha@qq.com>
2020-09-17 18:09:43 +0800

    stage packet.

--- Sdcb.FFmpegAPIWrapper/MediaCodecs/GeneratedTypes/Packet.stub.cs
+++ Sdcb.FFmpegAPIWrapper/MediaCodecs/GeneratedTypes/Packet.stub.cs
@@ -8,6 +8,38 @@ namespace Sdcb.FFmpegAPIWrapper.MediaCodecs
     public unsafe partial class Packet: FFmpegHandle
     {
         /// <summary>
+        /// <see cref="av_packet_alloc"/>
+        /// </summary>
+        public static Packet Create() => FromNative(av_packet_alloc(), isOwner: true);
+
+        /// <summary>
+        /// <see cref="av_packet_clone(AVPacket*)"/>
+        /// </summary>
+        /// <returns></returns>
+        public Packet Clone() => FromNative(av_packet_clone(this), isOwner: true);
+
+        /// <summary>
+        /// <see cref="av_init_packet(AVPacket*)"/>
+        /// <para>Note: Packet has already initialized in <see cref="Create"/>, call <see cref="Initialize"/> is not required after that.</para>
+        /// </summary>
+        private void Initialize() => av_init_packet(this);
+
+        /// <summary>
+        /// <see cref="av_new_packet(AVPacket*, int)"/>
+        /// </summary>
+        private void AllocBuffer(int size) => av_new_packet(this, size);
+
+        /// <summary>
+        /// <see cref="av_shrink_packet(AVPacket*, int)"/>
+        /// </summary>
+        public void Shink(int size) => av_shrink_packet(this, size);
+
+        /// <summary>
+        /// <see cref=""/>
+        /// </summary>
+        public void Grow(int growBy) => av_grow_packet(this, growBy);
+
+        /// <summary>
         /// <see cref="av_packet_free(AVPacket**)"/>
         /// </summary>
         public override void Close()
