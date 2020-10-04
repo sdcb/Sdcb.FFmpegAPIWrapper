df0d0fb089c6155860203c6fe0139f743ae93039
sdcb <sdflysha@qq.com>
2020-10-04 17:41:01 +0800

    Optimize FrameConverter.

--- Sdcb.FFmpegAPIWrapper/Swscales/FrameConverter.cs
+++ Sdcb.FFmpegAPIWrapper/Swscales/FrameConverter.cs
@@ -6,12 +6,18 @@ using FFmpeg.AutoGen;
 
 namespace Sdcb.FFmpegAPIWrapper.Swscales
 {
-    public unsafe class FrameConverter : PixelConverter
+    public unsafe class FrameConverter : FFmpegSafeObject
     {
+        public static implicit operator SwsContext*(FrameConverter data) => (SwsContext*)data._nativePointer;
+
         public FrameConverter(): base(IntPtr.Zero, isOwner: true)
         {
         }
 
+        /// <summary>
+        /// <see cref="sws_getCachedContext(SwsContext*, int, int, AVPixelFormat, int, int, AVPixelFormat, int, SwsFilter*, SwsFilter*, double*)"/>
+        /// <see cref="sws_scale(SwsContext*, byte*[], int[], int, int, byte*[], int[])"/>
+        /// </summary>
         public void ConvertFrame(Frame sourceFrame, Frame destFrame, ScaleFlag flags = ScaleFlag.None)
         {
             _nativePointer = NativeUtils.NotNull(
@@ -20,13 +26,26 @@ namespace Sdcb.FFmpegAPIWrapper.Swscales
                     destFrame.Width, destFrame.Height, (AVPixelFormat)destFrame.Format,
                     flags: (int)flags,
                     srcFilter: null, dstFilter: null, param: null),
-                message: ErrorMessage(
+                message: PixelConverter.ErrorMessage(
                     sourceFrame.Width, sourceFrame.Height, (PixelFormat)sourceFrame.Format,
                     destFrame.Width, destFrame.Height, (PixelFormat)destFrame.Format, flags));
 
-            Convert(
-                (Ptr4)sourceFrame.Data, (Int32x4)sourceFrame.Linesize, sourceFrame.Height, 
-                (Ptr4)destFrame.Data, (Int32x4)destFrame.Linesize);
+            sws_scale(this,
+                srcSlice: ((Ptr4)sourceFrame.Data).ToBytePtrArray(),
+                srcStride: ((Int32x4)sourceFrame.Linesize).ToArray(),
+                srcSliceY: 0, srcSliceH: sourceFrame.Width,
+                dst: ((Ptr4)destFrame.Data).ToBytePtrArray(), ((Int32x4)destFrame.Linesize).ToArray()).ThrowIfError();
         }
+
+        /// <summary>
+        /// <see cref="sws_freeContext(SwsContext*)"/>
+        /// </summary>
+        public void Free()
+        {
+            sws_freeContext(this);
+            _nativePointer = IntPtr.Zero;
+        }
+
+        protected override void DisposeNative() => Free();
     }
 }
---
+++ Sdcb.FFmpegAPIWrapper/linqpad-samples/swscale/swscale.linq
@@ -0,0 +1,9 @@
+<Query Kind="Statements">
+  <NuGetReference Prerelease="true">Sdcb.FFmpegAPIWrapper</NuGetReference>
+  <Namespace>FFmpeg.AutoGen</Namespace>
+  <Namespace>Sdcb.FFmpegAPIWrapper.MediaFormats</Namespace>
+  <Namespace>static FFmpeg.AutoGen.ffmpeg</Namespace>
+  <Namespace>System.Buffers.Binary</Namespace>
+  <Namespace>System.Runtime.CompilerServices</Namespace>
+</Query>
+
