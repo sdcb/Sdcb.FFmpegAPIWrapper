bca91ccd140432b053f32faf6a40806c44ca75c7
sdcb <sdflysha@qq.com>
2020-10-04 15:53:56 +0800

    Add GetLinesize/ToPlaneData

--- Sdcb.FFmpegAPIWrapper/MediaUtils/ImageUtils.cs
+++ Sdcb.FFmpegAPIWrapper/MediaUtils/ImageUtils.cs
@@ -33,5 +33,25 @@ namespace Sdcb.FFmpegAPIWrapper.MediaUtils
                 (AVPixelFormat)pixelFormat, 
                 width, height, align).ThrowIfError();
         }
+
+        /// <summary>
+        /// <see cref="av_image_fill_linesizes(ref int_array4, AVPixelFormat, int)"/>
+        /// </summary>
+        public static unsafe Int32x4 GetLinesize(PixelFormat pixelFormat, int width)
+        {
+            int_array4 linesizes;
+            av_image_fill_linesizes(ref linesizes, (AVPixelFormat)pixelFormat, width).ThrowIfError();
+            return linesizes;
+        }
+
+        /// <summary>
+        /// <see cref="av_image_fill_pointers(ref byte_ptrArray4, AVPixelFormat, int, byte*, int_array4)"/>
+        /// </summary>
+        public static unsafe Ptr4 ToPlaneData(IntPtr sourceData, Int32x4 linesizes, PixelFormat pixelFormat, int height)
+        {
+            byte_ptrArray4 ret;
+            av_image_fill_pointers(ref ret, (AVPixelFormat)pixelFormat, height, (byte*)sourceData, linesizes).ThrowIfError();
+            return ret;
+        }
     }
 }
--- Sdcb.FFmpegAPIWrapper/linqpad-samples/avcodec/avcodec.linq
+++ Sdcb.FFmpegAPIWrapper/linqpad-samples/avcodec/avcodec.linq
@@ -1,13 +1,12 @@
 <Query Kind="Statements">
   <NuGetReference Prerelease="true">Sdcb.FFmpegAPIWrapper</NuGetReference>
   <Namespace>FFmpeg.AutoGen</Namespace>
-  <Namespace>Sdcb.FFmpegAPIWrapper.MediaMuxers</Namespace>
+  <Namespace>Sdcb.FFmpegAPIWrapper.Common</Namespace>
+  <Namespace>Sdcb.FFmpegAPIWrapper.MediaCodecs</Namespace>
   <Namespace>static FFmpeg.AutoGen.ffmpeg</Namespace>
   <Namespace>System.Buffers.Binary</Namespace>
   <Namespace>System.Runtime.CompilerServices</Namespace>
-  <Namespace>Sdcb.FFmpegAPIWrapper.Common</Namespace>
   <Namespace>System.Runtime.InteropServices</Namespace>
-  <Namespace>Sdcb.FFmpegAPIWrapper.MediaCodecs</Namespace>
 </Query>
 
 new
