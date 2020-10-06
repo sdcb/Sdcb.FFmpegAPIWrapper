1d9d6fcebb38093ed15781eab081d9220d3eda84
sdcb <sdflysha@qq.com>
2020-10-06 21:57:57 +0800

    Add All/Encoders/Decoders/FindEncoders/FindDecoders.

--- Sdcb.FFmpegAPIWrapper/MediaCodecs/Codec_Static.cs
+++ Sdcb.FFmpegAPIWrapper/MediaCodecs/Codec_Static.cs
@@ -18,14 +18,20 @@ namespace Sdcb.FFmpegAPIWrapper.MediaCodecs
         /// <summary>
         /// <see cref="av_codec_iterate(void**)"/>
         /// </summary>
-        public static IEnumerable<Codec> MediaCodecs => NativeUtils
+        public static IEnumerable<Codec> All => NativeUtils
             .EnumeratePtrIterator(ptr => (IntPtr)(av_codec_iterate((void**)ptr)))
             .Select(FromNative);
+
+        public static IEnumerable<Codec> Encoders => All.Where(x => x.IsEncoder);
+
+        public static IEnumerable<Codec> Decoders => All.Where(x => x.IsDecoder);
 
         /// <summary>
         /// <see cref="avcodec_find_decoder(AVCodecID)"/>
         /// </summary>
         public static Codec FindDecoder(CodecID id) => FromNative(FindChecking(avcodec_find_decoder((AVCodecID)id), (AVCodecID)id));
+
+        public static IEnumerable<Codec> FindDecoders(CodecID id) => Decoders.Where(x => x.Id == id);
 
         /// <summary>
         /// <see cref="avcodec_find_decoder_by_name(string)"/>
@@ -36,6 +42,8 @@ namespace Sdcb.FFmpegAPIWrapper.MediaCodecs
         /// <see cref="avcodec_find_encoder(AVCodecID)"/>
         /// </summary>
         public static Codec FindEncoder(CodecID id) => FromNative(FindChecking(avcodec_find_encoder((AVCodecID)id), (AVCodecID)id));
+
+        public static IEnumerable<Codec> FindEncoders(CodecID id) => Encoders.Where(x => x.Id == id);
 
         /// <summary>
         /// <see cref="avcodec_find_encoder_by_name(string)"/>
