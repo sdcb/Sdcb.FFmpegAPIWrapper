fb8975a87f4f9c203ae5ececc52a46858f761cd4
sdcb <sdflysha@qq.com>
2022-02-15 09:27:46 +0800

    Add ModuleStartup back.

---
+++ Sdcb.FFmpegAPIWrapper/Common/ModuleStartup.cs
@@ -0,0 +1,16 @@
+﻿using FFmpeg.AutoGen;
+using System.Runtime.CompilerServices;
+
+namespace Sdcb.FFmpegAPIWrapper.Common
+{
+    internal class ModuleStartup
+    {
+#pragma warning disable CA2255 // 不应在库中使用 “ModuleInitializer” 属性
+        [ModuleInitializer]
+#pragma warning restore CA2255 // 不应在库中使用 “ModuleInitializer” 属性
+        public static void OnStart()
+        {
+            ffmpeg.RootPath = "";
+        }
+    }
+}
